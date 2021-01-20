fac47ed200cf5e50794e4faffa2ee95826391faf
Kurnia Muhamad <omenkzz@gmail.com>
2021-01-20 14:12:23 +0700

    Make the posts realtime

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
-    has_rich_text :content
+  has_rich_text :content
+
+  after_create_commit {broadcast_prepend_to "posts"}
+  after_update_commit {broadcast_replace_to "posts"}
+  after_destroy_commit {broadcast_remove_to "posts"}
 
 end
